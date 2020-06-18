6fb2c2662a7dd7d9e4ecf4d2ee6c9bd90a81d50f
septiannoor <septian.noor@gmail.com>
2020-06-18 17:59:34 +0700

    180620
    versi awal

---
+++ offline/offline_template.js
@@ -0,0 +1,3 @@
+dataxmlstr = hereDoc(function(){/*!
+	<learningObject editorVersion="3" targetFolder="Nottingham" name="Learning Object Title" language="" navigation="Linear" textSize="12" theme="default" displayMode="fill window" responsive="true" />
+*/});
